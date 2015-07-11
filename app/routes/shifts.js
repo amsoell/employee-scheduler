171004947aeb39f26eeab6ec991f061e422b079f
Andy Soell <andy@teamsoell.com>
2015-07-10 21:44:37 -0400

    Added "New" buttons to interface
    Also moved index rows into user-record and shift-record components

--- app/routes/shifts.js
+++ app/routes/shifts.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.find('shift');
+  },
+  actions: {
+    create: function() {
+      this.transitionTo('shifts.new');
+    }
   }
 });
