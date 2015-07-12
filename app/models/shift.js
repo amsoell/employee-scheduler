435a92a147d0671c241d5896d4e2ad48d64e10dd
Andy Soell <andy@teamsoell.com>
2015-07-12 15:45:06 -0400

    Added proper relationships in model definitions
    There's still a problem where associated objects aren't loaded unless they've been loaded in their primary page. For example, if you start on the /users page you won't see any shift data until you've already visited the /shifts page first, and vice versa.

--- app/models/shift.js
+++ app/models/shift.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   start_time:  DS.attr('string'),
   end_time:    DS.attr('string'),
-  employee_id: DS.attr('number'),
-  manager_id:  DS.attr('number'),
+  employee_id: DS.belongsTo('user', { async: true }),
+  manager_id:  DS.belongsTo('user', { async: true }),
   break:       DS.attr('number')
 });
--- app/models/user.js
+++ app/models/user.js
@@ -5,5 +5,17 @@ export default DS.Model.extend({
   email:    DS.attr('string'),
   phone:    DS.attr('string'),
   role:     DS.attr('string'),
-  password: DS.attr('string')
+  password: DS.attr('string'),
+  employee_shifts:   DS.hasMany('shift', { inverse: 'employee_id', async: true}),
+  manager_shifts:    DS.hasMany('shift', { inverse: 'manager_id', async: true }),
+  shifts: Ember.computed('employee_shifts', 'manager_shifts', function() {
+    var employee_shifts = this.get('employee_shifts') || [];
+    var manager_shifts = this.get('manager_shifts') || [];
+    var all_shifts = Ember.A();
+
+    all_shifts.pushObjects(employee_shifts.toArray());
+    all_shifts.pushObjects(manager_shifts.toArray());
+
+    return all_shifts;
+  })
 });
